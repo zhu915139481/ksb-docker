b72c2ad62a86f0ea7e3406713007e6f8d5bba427
侠客 <zhu915139481@126.com>
2017-08-04 16:02:03 +0800

    Update docker.sh

--- nignx-tomcat/docker.sh
+++ nignx-tomcat/docker.sh
@@ -4,46 +4,53 @@
 #date:2017年8月1日 18:46:26
 
 #设置SElinux
-setenforce 0
+#setenforce 0
+
+TOMCAT_CONF=/home/data/tomcats
+NGINX_CONF=/home/data/nginx
 
 #根据输入参数做处理
 case $1 in
   
   install )
+  
+    #添加selinux规则，改变要挂载的目录的安全性文本
+    chcon -Rt svirt_sandbox_file_t $NGINX_CONF $TOMCAT_CONF
+  
     #初始加载容器
 
     docker run \
         --name ccla-zsgl -h ccla-zsgl \
-        -v /home/data/tomcats/conf/server.xml:/usr/local/tomcat/conf/server.xml \
+        -v $TOMCAT_CONF/conf/server.xml:/usr/local/tomcat/conf/server.xml \
         -d tomcat:7.0.79
 
     docker run \
         --name ksb-front -h ksb-front \
-        -v /home/data/tomcats/conf/server.xml:/usr/local/tomcat/conf/server.xml \
+        -v $TOMCAT_CONF/conf/server.xml:/usr/local/tomcat/conf/server.xml \
         -d tomcat:7.0.79
 
     docker run \
         --name ksb-admin -h ksb-admin \
-        -v /home/data/tomcats/conf/server.xml:/usr/local/tomcat/conf/server.xml \
+        -v $TOMCAT_CONF/conf/server.xml:/usr/local/tomcat/conf/server.xml \
         -d tomcat:7.0.79
 
     docker run \
         --name ksb-weixin -h ksb-weixin \
-        -v /home/data/tomcats/conf/server.xml:/usr/local/tomcat/conf/server.xml \
+        -v $TOMCAT_CONF/conf/server.xml:/usr/local/tomcat/conf/server.xml \
         -d tomcat:7.0.79
 
     docker run \
         --name ksb-cgi -h ksb-cgi \
-        -v /home/data/tomcats/conf/server.xml:/usr/local/tomcat/conf/server.xml \
+        -v $TOMCAT_CONF/conf/server.xml:/usr/local/tomcat/conf/server.xml \
         -d tomcat:7.0.79
     
     docker rm -fv nginx
     
     docker run \
         --name nginx -h nginx \
-        -v /home/data/nginx/html:/usr/share/nginx/html \
-        -v /home/data/nginx/conf/nginx.conf:/etc/nginx/nginx.conf \
-        -v /home/data/nginx/conf/servers:/etc/nginx/servers \
+        -v $NGINX_CONF/html:/usr/share/nginx/html \
+        -v $NGINX_CONF/conf/nginx.conf:/etc/nginx/nginx.conf \
+        -v $NGINX_CONF/conf/servers:/etc/nginx/servers \
         --link ccla-zsgl:ccla-zsgl \
         --link ksb-front:ksb-front \
         --link ksb-admin:ksb-admin \
@@ -75,5 +82,3 @@ case $1 in
   ;;
 
 esac
-
-setenforce 1
