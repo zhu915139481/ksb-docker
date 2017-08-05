1a166330b1978fdada48717ec2c9309affee2bae
侠客 <zhu915139481@126.com>
2017-08-05 10:45:10 +0800

    Update docker.sh

--- nignx-tomcat/docker.sh
+++ nignx-tomcat/docker.sh
@@ -6,13 +6,19 @@
 #设置SElinux
 #setenforce 0
 
-TOMCAT_CONF=/home/data/tomcats
-NGINX_CONF=/home/data/nginx
-
 #oracle容器名称
 ORACLE_NAME=oracle
 #其他容器内联oracle主机名，用于连接oracle服务
 ORACLE_DB_NAME=oracle
+
+#配置文件
+TOMCAT_CONF=/home/data/tomcats
+NGINX_CONF=/home/data/nginx
+
+#nginx容器名称
+NGINX_NAME=nginx
+#所有的Tomcat名称
+TOMCAT_NAMES="ccla-zsgl ksb-front ksb-admin ksb-weixin ksb-cgi"
 
 #根据输入参数做处理
 case $1 in
@@ -21,51 +27,31 @@ case $1 in
   
     #添加selinux规则，改变要挂载的目录的安全性文本
     chcon -Rt svirt_sandbox_file_t $NGINX_CONF $TOMCAT_CONF
-  
+    
+    #tomcat容器连接
+    links=""
     #初始加载容器
+    for tomcat in $TOMCAT_NAMES
+    do
 
-    docker run \
-        --name ccla-zsgl -h ccla-zsgl \
+      docker run \
+        --name $tomcat -h $tomcat \
         -v $TOMCAT_CONF/conf/server.xml:/usr/local/tomcat/conf/server.xml \
         --link $ORACLE_NAME=$ORACLE_DB_NAME \
         -d tomcat:7.0.79
+      
+      links=$links --link $tomcat:$tomcat
 
-    docker run \
-        --name ksb-front -h ksb-front \
-        -v $TOMCAT_CONF/conf/server.xml:/usr/local/tomcat/conf/server.xml \
-        --link $ORACLE_NAME=$ORACLE_DB_NAME \
-        -d tomcat:7.0.79
-
-    docker run \
-        --name ksb-admin -h ksb-admin \
-        -v $TOMCAT_CONF/conf/server.xml:/usr/local/tomcat/conf/server.xml \
-        --link $ORACLE_NAME=$ORACLE_DB_NAME \
-        -d tomcat:7.0.79
-
-    docker run \
-        --name ksb-weixin -h ksb-weixin \
-        -v $TOMCAT_CONF/conf/server.xml:/usr/local/tomcat/conf/server.xml \
-        --link $ORACLE_NAME=$ORACLE_DB_NAME \
-        -d tomcat:7.0.79
-
-    docker run \
-        --name ksb-cgi -h ksb-cgi \
-        -v $TOMCAT_CONF/conf/server.xml:/usr/local/tomcat/conf/server.xml \
-        --link $ORACLE_NAME=$ORACLE_DB_NAME \
-        -d tomcat:7.0.79
+    done
     
-    docker rm -fv nginx
+    docker rm -fv $NGINX_NAME
     
     docker run \
-        --name nginx -h nginx \
+        --name $NGINX_NAME -h $NGINX_NAME \
         -v $NGINX_CONF/html:/usr/share/nginx/html \
         -v $NGINX_CONF/conf/nginx.conf:/etc/nginx/nginx.conf \
         -v $NGINX_CONF/conf/servers:/etc/nginx/servers \
-        --link ccla-zsgl:ccla-zsgl \
-        --link ksb-front:ksb-front \
-        --link ksb-admin:ksb-admin \
-        --link ksb-weixin:ksb-weixin \
-        --link ksb-cgi:ksb-cgi \
+        $links \
         -p 80:80 \
         -d nginx
 
@@ -73,22 +59,22 @@ case $1 in
 
   start )
     #启动容器
-    docker start ccla-zsgl ksb-front ksb-admin ksb-weixin ksb-cgi
+    docker start $TOMCAT_NAMES
   ;;
 
   stop )
     #关闭容器
-    docker stop ccla-zsgl ksb-front ksb-admin ksb-weixin ksb-cgi
+    docker stop $TOMCAT_NAMES
   ;;
 
   restart )
     #重启容器
-    docker restart ccla-zsgl ksb-front ksb-admin ksb-weixin ksb-cgi
+    docker restart $TOMCAT_NAMES
   ;;
 
   delete )
     #删除容器
-    docker rm -fv ccla-zsgl ksb-front ksb-admin ksb-weixin ksb-cgi
+    docker rm -fv $TOMCAT_NAMES
   ;;
 
 esac
